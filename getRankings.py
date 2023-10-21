b9f0f2710b9bd043f0d1edffec98c2aea786dcf0
Shawn Xue <shawnxue.c@gmail.com>
2023-10-21 13:12:30 -0400

    pooooosh

--- getRankings.py
+++ getRankings.py
@@ -11,18 +11,12 @@ def calculate_expected_result(rating_a, rating_b):
     return 1 / (1 + 10 ** ((rating_b - rating_a) / 400))
 
 
-def update_ratings(rating_a, rating_b, teamOneGames, teamTwoGames, outcome, stage, k=32):
+def update_ratings(rating_a, rating_b, teamOneGames, teamTwoGames, outcome, scale):
     """
     Update Elo ratings for two players based on the outcome of a match.
     """
     expected_a = calculate_expected_result(rating_a, rating_b)
     expected_b = 1 - expected_a
-    
-    scale = 1
-
-    if stage == 'playoffs' or stage == 'knockout':
-       scale = 5
-
 
     new_rating_a = rating_a + scale * (50/(1+(teamOneGames / 300))) * (outcome - expected_a)
     new_rating_b = rating_b + scale * (50/(1+(teamTwoGames / 300))) * ((1 - outcome) - expected_b)
@@ -30,27 +24,41 @@ def update_ratings(rating_a, rating_b, teamOneGames, teamTwoGames, outcome, stag
     return new_rating_a, new_rating_b
 
 
+def get_prio(tournamentId):
+  with open('esports-data/leagues.json', 'r') as json_file:
+    leagues = json.load(json_file)
+
+    for league in leagues:
+      prio = league.get("priority")
+      tournaments = league.get("tournaments")
+      for tourn in tournaments:
+        if tourn.get("id") == tournamentId:
+          return prio
+
+  
+  return 1000
+
 teams = []
 initial_ratings = []
-for tournaments in os.listdir('games')[1:4]:
-  for file in os.listdir('games/' + tournaments):
-    with open('games/'+tournaments+'/'+file, 'r') as json_file:
-        tour = json.load(json_file)
+with open('tournaments_game_info.json', 'r') as json_file:
+    tour = json.load(json_file)
 
-    teamOne = tour.get('teamOne')
-    teamTwo = tour.get('teamTwo')
-    if teamOne not in teams:
-        teams.append(teamOne)
-        adjFactor = tour.get('priority')
-        if adjFactor < 10: adjFactor = 1
-        elif 100 < adjFactor: adjFactor = 10
-        initial_ratings.append(1500 / adjFactor)
+for game in tour: 
+  teamOne = game.get('teamOne')
+  teamTwo = game.get('teamTwo')
+  if teamOne not in teams:
+      teams.append(teamOne)
+      adjFactor = get_prio(game.get("tournamentId"))
+      if adjFactor < 10: adjFactor = 1
+      elif 100 < adjFactor: adjFactor = 10
+      initial_ratings.append(1500 / adjFactor)
 
-    if teamTwo not in teams:
-        teams.append(teamTwo)
-        if adjFactor < 10: adjFactor = 1
-        elif 100 < adjFactor: adjFactor = 10
-        initial_ratings.append(1500 / adjFactor)
+  if teamTwo not in teams:
+      teams.append(teamTwo)
+      if adjFactor < 10: adjFactor = 1
+      elif 100 < adjFactor < 1000: adjFactor = 10
+      else: adjFactor = 50
+      initial_ratings.append(1500 / adjFactor)
 
 total_games = np.full(
   shape=len(teams),
@@ -60,24 +68,23 @@ total_games = np.full(
 # Create a DataFrame to store ratings
 df = pd.DataFrame({"team": teams, "Rating": initial_ratings, "gamesPlayed": total_games})
 
-for tournaments in os.listdir('FilteredData'):
-  with open('FilteredData/'+tournaments, 'r') as json_file:
-    results = json.load(json_file)
-  for tour in results:
-      teamOne = tour.get("teamOne")
-      teamTwo = tour.get("teamTwo")
-      stage = tour.get("stage")
-      outcome = 1 if tour.get("winningTeam") == teamOne else 0
+with open('tournaments_game_info.json', 'r') as json_file:
+  results = json.load(json_file)
+for tour in results:
+    teamOne = tour.get("teamOne")
+    teamTwo = tour.get("teamTwo")
+    weight = tour.get("weight")
+    outcome = 1 if tour.get("winTeam") == teamOne else 0
 
-      new_rating_one, new_rating_two = update_ratings(df.loc[df["team"] == teamOne, "Rating"].values[0],
-                                                      df.loc[df["team"] == teamTwo, "Rating"].values[0],
-                                                      df.loc[df["team"] == teamTwo, "gamesPlayed"].values[0],
-                                                      df.loc[df["team"] == teamTwo, "gamesPlayed"].values[0], outcome, stagew)
+    new_rating_one, new_rating_two = update_ratings(df.loc[df["team"] == teamOne, "Rating"].values[0],
+                                                    df.loc[df["team"] == teamTwo, "Rating"].values[0],
+                                                    df.loc[df["team"] == teamTwo, "gamesPlayed"].values[0],
+                                                    df.loc[df["team"] == teamTwo, "gamesPlayed"].values[0], outcome, weight)
 
-      df.loc[df["team"] == teamOne, "Rating"] = new_rating_one
-      df.loc[df["team"] == teamOne, "gamesPlayed"] = df.loc[df["team"] == teamOne, "gamesPlayed"].values[0] + 1
-      df.loc[df["team"] == teamTwo, "Rating"] = new_rating_two
-      df.loc[df["team"] == teamTwo, "gamesPlayed"] = df.loc[df["team"] == teamTwo, "gamesPlayed"].values[0] + 1
+    df.loc[df["team"] == teamOne, "Rating"] = new_rating_one
+    df.loc[df["team"] == teamOne, "gamesPlayed"] = df.loc[df["team"] == teamOne, "gamesPlayed"].values[0] + 1
+    df.loc[df["team"] == teamTwo, "Rating"] = new_rating_two
+    df.loc[df["team"] == teamTwo, "gamesPlayed"] = df.loc[df["team"] == teamTwo, "gamesPlayed"].values[0] + 1
 
 
 returnDict = df.to_dict('records')
@@ -92,6 +99,6 @@ for val in returnDict:
             val.update({"name": pro.get("name")})
 
 print(returnDict)
-with open("randomLeague_rankings.json", "w") as outfile:
+with open("test_rankings.json", "w") as outfile:
     json.dump(
-        sorted(returnDict, key=lambda x: x['Rating'], reverse=True), outfile)
+        sorted(returnDict, key=lambda x: x['Rating'], reverse=True), outfile, indent=2)
